389d6b20c44216f4f350620a441f60b22c4892fe
karinacamposal <2109020@upy.edu.mx>
2022-04-08 14:32:47 -0500

    Sort Array code

---
+++ sortArray.h
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int sort (){
+   int arr[20];
+   int i, j, a, n;
+   printf("enter number of elements in an array\n");
+   scanf("%d", &n);
+   printf("Enter the elements\n");
+   for (i = 0; i < n; ++i)
+      scanf("%d", &arr[i]);
+   for (i = 0; i < n; ++i){
+      for (j = i + 1; j < n; ++j){
+         if (arr[i] > arr[j]){
+            a = arr[i];
+            arr[i] = arr[j];
+            arr[j] = a;
+         }
+      }
+   }
+   printf("The numbers in ascending order is:\n");
+   for (i = 0; i < n; ++i){
+      printf("%d\n", arr[i]);
+   }
+}
