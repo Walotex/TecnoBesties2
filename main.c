158c42791b0cee76209568396427a986e329d9ac
danycuso <danyvartiles1@gmail.com>
2022-04-08 16:31:57 -0500

    commit

--- main.c
+++ main.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "print.h"
-#include "sortArray.h"
 #include "find.h"
 
 int arr[] = {9,8,7,6,5,4,3,2,1};
@@ -13,9 +11,8 @@ int look = 52;
 int main(){
 	system("clear");
 
-	int sortArray = sort(arr, temp, len);
 
-printf("Find values: %d\n", find(arr, 5, 10));
+printf("Find values: %d\n", find(arr, len, 7));
 	
 
 	return 0;
--- print.h
+++
@@ -1,5 +0,0 @@
-void print_array (int array [], int len){
-    printf("{");
-    for (int i= 0; i< len; i++) printf("%d ", array[i]);
-    printf("}\n");
-}
